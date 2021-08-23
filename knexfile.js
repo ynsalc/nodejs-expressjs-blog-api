b15dfb347535ac2c8d8ddf7ceffde5556ba98b9f
Yunus Alıcı <yunusac51@gmail.com>
2021-08-23 03:19:50 +0300

    update-knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,11 @@ module.exports = {
     development: {
       client: "pg",
       connection: {
-        database: "blogproject",
-        user: "postgres",
-        password: "123456",
+        database: "dvt9ppubaf2bd",
+        user: "rgkgagyhmbtejm",
+        password: "6926269d2e00997dec2aaf6dc26f2b835b5087026585949aa5c710641c2f40ad",
+		host:"ec2-44-196-170-156.compute-1.amazonaws.com",
+		port:"5432"
       },
       migrations: {
         directory: "./data/migrations",
